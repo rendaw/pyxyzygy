f53f650abb47381bd2433af4cf83737e7e1f2081
rendaw <>
2019-01-27 05:36:44 +0900

    Testing build

--- native/build.py
+++ native/build.py
@@ -9,7 +9,6 @@ def main():
     package = ['com', 'zarbosoft', 'shoedemo']
 
     os.chdir(Path(__file__).parent)
-    java = Path('/usr/lib/jvm/java-8-openjdk')
     java_dest = Path() / '..' / 'app' / 'target' / 'src' / '/'.join(package)
     os.makedirs(java_dest, exist_ok=True)
     java_resource_dest = (
@@ -30,33 +29,60 @@ def main():
         'header.hpp',
     ])
 
-    def compile_obj(name):
+    general_flags = [
+        '-static-libstdc++',
+        '-Wall', '-pedantic',
+        '-O3',
+    ]
+
+    def compile(key, cc, suffix, includes, libs):
+        def compile_obj(name):
+            c([
+                cc,
+            ] + general_flags + [
+                '-c',
+                '-o', '{}.{}.o'.format(name, key),
+                name,
+            ] + includes)
+
+        compile_obj('header_wrap.cxx')
+        compile_obj('implementation.cxx')
         c([
-            'clang++',
-            '-c',
-            '-Wall', '-pedantic',
-            '-O3',
-            '-o', '{}.o'.format(name),
-            name,
-            '-I/c/jdk1.3.1/include',
-            '-I/c/jdk1.3.1/include/win32',
-            '-I/usr/include',
-            '-I' + str(java / 'include'),
-            '-I' + str(java / 'include/linux'),
+            cc,
+        ] + general_flags + libs + [
+            '-shared',
+            '-L/usr/lib',
+            '-o', java_resource_dest / '{}.{}'.format(module, suffix),
+            'header_wrap.cxx.lx64.o',
+            'implementation.cxx.lx64.o',
+            '-lpng',
         ])
 
-    compile_obj('header_wrap.cxx')
-    compile_obj('implementation.cpp')
-    c([
+    compile(
+        'lx64',
         'clang++',
-        '-shared',
-        '-O3',
-        '-L/usr/lib',
-        '-o', java_resource_dest / '{}.so'.format(module),
-        'header_wrap.cxx.o',
-        'implementation.cpp.o',
-        '-lpng',
-    ])
-
+        'so',
+        [
+            '-I/usr/include',
+            '-I/usr/lib/jvm/java-11-openjdk/include',
+            '-I/usr/lib/jvm/java-11-openjdk/include/linux',
+        ],
+        [
+            '-L/usr/lib',
+        ],
+    )
+    compile(
+        'wx64',
+        'x86_64-w64-mingw32-g++',
+        'dll',
+        [
+            '-I/usr/i686-w64-mingw32/include',
+            '-I/home/andrew/temp/ren/shoedemo2/temp/jdk-11.0.2/include',
+            '-I/home/andrew/temp/ren/shoedemo2/temp/jdk-11.0.2/include/win32',
+        ],
+        [
+            '-L/usr/lib/gcc/i686-w64-mingw32/',
+        ],
+    )
 
 main()
