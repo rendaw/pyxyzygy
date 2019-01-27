52be504fb0755955d84479f90c0c9464453a7c6e
rendaw <>
2019-01-28 02:59:43 +0900

    Build changes

--- graphics/build.py
+++ graphics/build.py
@@ -9,10 +9,20 @@ os.makedirs(resources, exist_ok=True)
 source_icons = Path.cwd() / 'icons'
 
 for icon in source_icons.glob('*.svg'):
-    subprocess.check_call([
-        'inkscape',
-        icon,
-        '-z',
-        '-e', resources / (icon.stem + '.png'),
-        '-w', '16', '-h', '16',
-    ])
+    if icon.startswith('appicon'):
+        sizes = [16, 32, 64]
+    else:
+        sizes = [16]
+    for size in sizes:
+        size = str(size)
+        name = icon.stem
+        if sizes:
+            name = name + size
+        name = name + '.png'
+        subprocess.check_call([
+            'inkscape',
+            icon,
+            '-z',
+            '-e', resources / name,
+            '-w', size, '-h', size,
+        ])
--- native/build.py
+++ native/build.py
@@ -16,6 +16,11 @@ def main():
     )
     os.makedirs(java_resource_dest, exist_ok=True)
 
+    win_java = os.environ['BUILD_WIN_JAVA']
+    win_include = os.environ['BUILD_WIN_INCLUDE']
+    win_lib = os.environ['BUILD_WIN_LIB']
+    win_cxx = os.environ['BUILD_WIN_CXX']
+
     def c(*pargs, **kwargs):
         print(*pargs, **kwargs)
         check_call(*pargs, **kwargs)
@@ -30,37 +35,28 @@ def main():
     ])
 
     general_flags = [
-        '-static-libstdc++',
         '-Wall', '-pedantic',
         '-O3',
     ]
 
     def compile(key, cc, suffix, includes, libs):
-        def compile_obj(name):
-            c([
-                cc,
-            ] + general_flags + [
-                '-c',
-                '-o', '{}.{}.o'.format(name, key),
-                name,
-            ] + includes)
-
-        compile_obj('header_wrap.cxx')
-        compile_obj('implementation.cxx')
         c([
             cc,
-        ] + general_flags + libs + [
+        ] + general_flags + includes + libs + [
             '-shared',
-            '-L/usr/lib',
+            '-fPIC',
+            '-static-libgcc',
+            '-static-libstdc++',
             '-o', java_resource_dest / '{}.{}'.format(module, suffix),
-            'header_wrap.cxx.lx64.o',
-            'implementation.cxx.lx64.o',
+            'header_wrap.cxx',
+            'implementation.cxx',
             '-lpng',
+            '-lz',
         ])
 
     compile(
         'lx64',
-        'clang++',
+        'g++',
         'so',
         [
             '-I/usr/include',
@@ -73,16 +69,18 @@ def main():
     )
     compile(
         'wx64',
-        'x86_64-w64-mingw32-g++',
+        win_cxx,
         'dll',
         [
-            '-I/usr/i686-w64-mingw32/include',
-            '-I/home/andrew/temp/ren/shoedemo2/temp/jdk-11.0.2/include',
-            '-I/home/andrew/temp/ren/shoedemo2/temp/jdk-11.0.2/include/win32',
+            f'-I{win_include}',
+            f'-I{win_java}/include',
+            f'-I{win_java}/include/win32',
         ],
         [
-            '-L/usr/lib/gcc/i686-w64-mingw32/',
+            f'-L{win_lib}',
+            '-static',
         ],
     )
+
 
 main()
