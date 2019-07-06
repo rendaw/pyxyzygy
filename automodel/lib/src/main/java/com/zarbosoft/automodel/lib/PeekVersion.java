93db3e423c9450ecedc60574d22f12d460e3280e
rendaw <>
2019-07-07 05:11:52 +0900

    Fix build eventually

--- automodel/lib/src/main/java/com/zarbosoft/automodel/lib/PeekVersion.java
+++ automodel/lib/src/main/java/com/zarbosoft/automodel/lib/PeekVersion.java
@@ -3,24 +3,25 @@ package com.zarbosoft.automodel.lib;
 import com.zarbosoft.luxem.events.LTypeEvent;
 import com.zarbosoft.luxem.read.BufferedRawReader;
 import com.zarbosoft.luxem.read.RawReader;
+import com.zarbosoft.rendaw.common.Pair;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static com.zarbosoft.rendaw.common.Common.uncheck;
-
 public class PeekVersion {
   public static String check(Path path) {
-    return uncheck(
-        () -> {
-          try (InputStream source = Files.newInputStream(path)) {
-            return BufferedRawReader.streamEvents(source, new RawReader.DefaultEventFactory())
-                .findFirst()
-                .filter(p -> p.first instanceof LTypeEvent)
-                .map(p -> ((LTypeEvent) p.first).value)
-                .orElse(null);
-          }
-        });
+    try (InputStream source = Files.newInputStream(path)) {
+      return BufferedRawReader.streamEvents(source, new RawReader.DefaultEventFactory())
+          .map(p -> p.first)
+          .findFirst()
+          .filter(e -> e instanceof LTypeEvent)
+          .map(e -> ((LTypeEvent) e).value)
+          .orElse(null);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
   }
 }
